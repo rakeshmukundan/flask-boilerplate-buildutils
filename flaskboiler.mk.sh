c5bfc62f07d5e628e777744f53f9e1c411ca1267
Nick Whyte <nick@nickwhyte.com>
2015-02-26 15:50:43 +1100

    Makefiles are back !

--- flaskboiler.mk.sh
+++ flaskboiler.mk.sh
@@ -2,15 +2,16 @@ VENV_LOCATION=.venv
 VENV_ACTIVATE="$(VENV_LOCATION)/bin/activate"
 
 debug: venv
-	. .env; python run.py $(ARGS) --debug
-
+	. .env && python run.py server $(ARGS) --debug
 
 venv: $(VENV_LOCATION)
 
-$(VENV_LOCATION): Application/requirements.txt
-	. .env;
-	touch $(VENV_LOCATION)
+$(VENV_LOCATION): Application/requirements*.txt
+	. .env && pip3 install -r Application/requirements.txt --upgrade
+	touch $(VENV_LOCATION);
+	$(eval CONFIG_CLASS := $(shell . .env && python3 -m config))
+	test -e 'Application/requirements-$(CONFIG_CLASS).txt' && pip3 install -r 'Application/requirements-$(CONFIG_CLASS).txt' --upgrade
 
 
-uninstall:
+clean:
 	rm -rf $(VENV_LOCATION)
--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
     'virtualenv',
     'lesscpy'
   ],
-  scripts=['flaskenv.sh', 'flaskboiler.mk']
+  scripts=['flaskenv.sh', 'flaskboiler.mk.sh']
 )
