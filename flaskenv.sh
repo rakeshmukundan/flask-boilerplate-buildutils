26c60cbfc1fa07e9b12a1d28de21db881311f07c
Nick Whyte <nick@nickwhyte.com>
2015-02-26 16:41:23 +1100

    Specify python3.4

--- flaskenv.sh
+++ flaskenv.sh
@@ -7,14 +7,14 @@ fi
 if [ -e .venv/bin/activate ]; then
     . .venv/bin/activate;
 else
-    virtualenv -p `which python3` .venv;
+    virtualenv -p `which python3.4` .venv;
     . .venv/bin/activate;
     upgrade;
 fi
 
 upgrade() {
     pip3 install -r reqs/requirements.txt --upgrade;
-    CONFIG_CLASS=`python3 -m config`
+    CONFIG_CLASS=`python3.4 -m config`
     test -e "reqs/requirements-${CONFIG_CLASS}.txt" && 
     pip3 install -r "reqs/requirements-${CONFIG_CLASS}.txt" --upgrade
 }
